17eed2ba00bdcc70f92e53b4d67c4f0185223fdd
Damilola Adegunwa <dammy.adegunwa@gmail.com>
2022-12-06 15:30:52 +0100

    ☕ minor adjustment to some entities 📔📔

--- src/Domain/Entities/GroupAggregate/Events/GroupCreatedEvent.cs
+++ src/Domain/Entities/GroupAggregate/Events/GroupCreatedEvent.cs
@@ -8,3 +8,13 @@ namespace ArrayApp.Domain.Entities.GroupAggregate.Events;
 internal class GroupCreatedEvent
 {
 }
+/*
+ Here are some examples of events that could be associated with a Group class in C#:
+
+GroupCreatedEvent: This event is raised when a new group is created. This could be used to add the group to a list of groups and update the user interface to reflect the change.
+GroupDeletedEvent: This event is raised when a group is deleted. This could be used to remove the group from the list of groups and update the user interface to reflect the change.
+GroupRenamedEvent: This event is raised when a group is renamed. This could be used to update the group's name in the list of groups and update the user interface to reflect the change.
+GroupMemberAddedEvent: This event is raised when a member is added to a group. This could be used to update the list of members in the group and update the user interface to reflect the change.
+GroupMemberRemovedEvent: This event is raised when a member is removed from a group. This could be used to update the list of members in the group and update the user interface to reflect the change.
+These are just a few examples of events that could be associated with a Group class in C#. There may be other events available depending on the specific implementation of the Group class.
+ */
--- src/Domain/Entities/SubscriptionAggregate/Subscription.cs
+++ src/Domain/Entities/SubscriptionAggregate/Subscription.cs
@@ -3,8 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ArrayApp.Domain.Entities.AdvertAggregate;
 
 namespace ArrayApp.Domain.Entities.SubscriptionAggregate;
-internal class Subscription
+public class Subscription
 {
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public SubscriptionStatus Status { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public User User { get; set; }
+    public Product Product { get; set; }
+    public decimal Price { get; set; }
+    public int RenewalInterval { get; set; }
+    public bool AutoRenew { get; set; }
+}
+
+public enum SubscriptionStatus
+{
+    Active,
+    Expired,
+    Cancelled
+}
+
+public enum PaymentMethod
+{
+    CreditCard,
+    BankTransfer,
+    PayPal
 }
